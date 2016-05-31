4d474df1192f60628861547ff8129d5263abf56f
battez <j.l.p.barker@dundee.ac.uk>
2016-05-31 19:29:34 +0100

    add priority of 25 locations to script

--- twy_tweet_stream.py
+++ twy_tweet_stream.py
@@ -1,14 +1,6 @@
 '''stream a set of users current tweets into a database'''
-
-# get the Twitter API app Oauth tokens
-import sys
-TWITDIR = 'U:\Documents\Project\demoapptwitter'
-sys.path.insert(0, TWITDIR)
-import config
-
 # Thread as the consumer of the Twitter stream will run in a separate thread.
 from threading import Thread
-import threading
 
 # provide a lock-free way to move messages from one thread to another.
 from queue import Queue
@@ -27,23 +19,37 @@ import logging
 FORMAT = "%(asctime)-15s %(message)s"
 logging.basicConfig(filename="log.txt", level=logging.INFO, format=FORMAT)
 
+# get the Twitter API app Oauth tokens
+import sys
+TWITDIR = 'U:\Documents\Project\demoapptwitter'
+sys.path.insert(0, TWITDIR)
+import config
+
+# get fn updating water levels:
+SCRAPEDIR = 'U:\Documents\Project\scrape'
+sys.path.insert(0, SCRAPEDIR)
+import parse_xml_locs, retrieve_latest_from_csv
 
 # implement our extension to Twythons streamer
 class TwitterStream(TwythonStreamer):
     '''Set up a queue for the tweets. Provides information on what to do
      when a tweet is received and on errors.'''
-    def __init__(self, consumer_key, consumer_secret, token, token_secret, tqueue):
+    def __init__(self, consumer_key, consumer_secret, token, token_secret, \
+        tqueue):
         self.tweet_queue = tqueue
-        self._stopevent = threading.Event( )
+        # FIXME: use in case of threading
+        # self._stopevent = threading.Event( )
+
         # pass credentials to the parent class for authenticating:
-        super(TwitterStream, self).__init__(consumer_key, consumer_secret, token, token_secret)
+        super(TwitterStream, self).__init__(consumer_key, consumer_secret, \
+            token, token_secret)
 
     def on_success(self, data):
         if 'text' in data:
             self.tweet_queue.put(data)
 
     def on_error(self, status_code, data):
-        print(status_code)
+        logging.warning('code: ' + status_code)
         
         # Uncomment to stop trying to get data because of the error
         # self.disconnect()
@@ -51,7 +57,7 @@ class TwitterStream(TwythonStreamer):
 
 def stream_tweets(tweets_queue, querystring, filter='follow'):
     ''' samples the stream API and handles errors'''
-    
+    print(querystring)
     # OAuth credentials 
     consumer_key = config.ul_consumer_key
     consumer_secret = config.ul_consumer_secret
@@ -59,7 +65,8 @@ def stream_tweets(tweets_queue, querystring, filter='follow'):
     token_secret = config.ul_access_secret
 
     try:
-        stream = TwitterStream(consumer_key, consumer_secret, token, token_secret, tweets_queue)
+        stream = TwitterStream(consumer_key, consumer_secret, token, \
+            token_secret, tweets_queue)
         # filter on users / keywords
         if (filter == 'follow'):
             stream.statuses.filter(follow=querystring)
@@ -69,7 +76,8 @@ def stream_tweets(tweets_queue, querystring, filter='follow'):
             stream.statuses.filter(locations=querystring, language='en')
 
     except ChunkedEncodingError:
-        # Sometimes the API sends back one byte less than expected which results in an exception in the
+        # Sometimes the API sends back one byte less than expected which 
+        # results in an exception in the
         # current version of the requests library
         stream_tweets(tweet_queue)
 
@@ -84,7 +92,7 @@ def process_tweets(dbc, tweets_queue):
             tweet = tweets_queue.get()
             count += 1
         else:
-            print('max reached, or timer expired - num. collected:' + str(count))
+            print('max reached, or timer expired; num. collected:' + str(count))
             
             break
 
@@ -97,6 +105,7 @@ def process_tweets(dbc, tweets_queue):
         tweets_queue.task_done()
 
     return
+
 
 def get_dbc(db_name, collection, host='localhost:27017'):
     '''Convenience wrapper for connecting to a collection 
@@ -115,12 +124,12 @@ def get_dbc(db_name, collection, host='localhost:27017'):
 if __name__ == '__main__':
     ''' run the script, be sure to adjust the query 
     as needed'''
-
-    logging.info('started')
+    logging.info('Task started')
 
     # choose which filter and query will be streamed
     filters = ['follow', 'locations', 'track']
     query = ''
+
     # set the Twitter Stream type of query here:
     filtering = filters[1]
 
@@ -139,12 +148,41 @@ if __name__ == '__main__':
         query = ",".join(users)
     
     elif (filtering == 'locations'):
-        # FIXME: take the prioritised tweets:
+
         dbc = get_dbc('Twitter', 'gauges')
-        print('sampling...')
-        sampled = dbc.aggregate([{'$sample':{'size':25}}])
+        
+        print('processing gauge levels...')
+        # get all gauges averages:
+        gauges = dbc.find({}, {'loc_id':1,'avg_level':1, '_id':0})
+        
+        # retrieve the location IDs of all the gauges
+        ids = [gauge['loc_id'] for gauge in gauges]
+        
+        # get all the current levels from web CSVs 
+        # debug:
+        # latest_levels = {'116008':'0.01', '133112': \
+        # '0.239', '234268': '5.224', '372871': '0.075'}
+        latest_levels = retrieve_latest_from_csv.scrape_current_levels(ids) 
+        
+        excluded_gauges = ['116008'] # some seem to be wrongly calibrated!
+
+        for gauge in gauges.rewind():
+                
+            if (gauge['loc_id'] in latest_levels) and \
+            (gauge['loc_id'] not in excluded_gauges):
+                current_scaled = float(latest_levels[gauge['loc_id']]) / \
+                float(gauge['avg_level'])
+                current_scaled = float("{0:.4f}".format(current_scaled))
+                dbc.update({'loc_id': gauge['loc_id']}, {'$set': \
+                    {'current_scaled': current_scaled, \
+                    'current':latest_levels[gauge['loc_id']] }})
+                
+        # now take the n highest levels and watch these locations with Twitter:
+        observed = dbc.find({}, { 'current_scaled': 1, 'bounding_box': 1, \
+            'loc_id': 1, '_id': 0 }).sort([('current_scaled', -1)]).limit(25)
+
         prefix = ''
-        for document in sampled:
+        for document in observed:
         
             coords = [str(coord) for coord in document['bounding_box']]
             query += prefix + ','.join(coords)
